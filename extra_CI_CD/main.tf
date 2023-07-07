fe0931403fe397728925fa035bacafa16a98870b
Moenes Ben Soussia <63344830+moenesbs@users.noreply.github.com>
2023-07-07 15:33:33 +0200

    Update main.tf

--- extra_CI_CD/main.tf
+++ extra_CI_CD/main.tf
@@ -8,6 +8,45 @@ resource "random_pet" "name" {
   length = 2
 }
 
-resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+
+
+resource "aws_iam_role" "notebook_role" {
+  name = "sagemaker_notebook_role-${var.student_name}"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Service = "sagemaker.amazonaws.com"
+        }
+      },
+    ]
+  })
+  managed_policy_arns = []
+
+  inline_policy {
+    name = "S3_access"
+    policy = jsonencode({
+      Version = "2012-10-17"
+      Statement = [
+        {
+          Action   = ["s3:ListAllMyBuckets", "s3:ListBucket", "s3:HeadBucket"]
+          Effect   = "Allow"
+          Resource = "*"
+        },
+      ]
+    })
+  }
 }
+
+
+resource "aws_sagemaker_notebook_instance" "notebook_instance" {
+  name                   = "example-notebook-instance"
+  instance_type          = "ml.t2.medium"
+  role_arn               = aws_iam_role.notebook_role.arn
+}
+
+
