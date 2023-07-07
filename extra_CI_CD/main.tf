051e8996c58cf647a04b51eb59c0307140e13771
Moenes Ben Soussia <63344830+moenesbs@users.noreply.github.com>
2023-07-07 15:39:32 +0200

    Update main.tf

--- extra_CI_CD/main.tf
+++ extra_CI_CD/main.tf
@@ -8,44 +8,28 @@ resource "random_pet" "name" {
   length = 2
 }
 
-
-
-resource "aws_iam_role" "notebook_role" {
-  name = "sagemaker_notebook_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "sagemaker.amazonaws.com"
-        }
-      },
-    ]
-  })
-  managed_policy_arns = []
-
-  inline_policy {
-    name = "S3_access"
-    policy = jsonencode({
-      Version = "2012-10-17"
-      Statement = [
-        {
-          Action   = ["s3:ListAllMyBuckets", "s3:ListBucket", "s3:HeadBucket"]
-          Effect   = "Allow"
-          Resource = "*"
-        },
-      ]
-    })
+data "aws_ami" "this" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "architecture"
+    values = ["arm64"]
+  }
+  filter {
+    name   = "name"
+    values = ["al2023-ami-2023*"]
   }
 }
 
-resource "aws_sagemaker_notebook_instance" "notebook_instance" {
-  name                   = "example-notebook-instance"
-  instance_type          = "ml.p3.16xlarge"
-  role_arn               = aws_iam_role.notebook_role.arn
+resource "aws_instance" "this" {
+  ami = data.aws_ami.this.id
+  instance_market_options {
+    spot_options {
+      max_price = 0.0031
+    }
+  }
+  instance_type = "t4g.nano"
+  tags = {
+    Name = "test-spot"
+  }
 }
-
-
