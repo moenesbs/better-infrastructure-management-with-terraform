6fc5d10c3f10ceb8d8e0e657c119cb0a0b6dc2cb
Moenes Ben Soussia <63344830+moenesbs@users.noreply.github.com>
2023-07-07 15:41:33 +0200

    Update main.tf

--- extra_CI_CD/main.tf
+++ extra_CI_CD/main.tf
@@ -8,28 +8,26 @@ resource "random_pet" "name" {
   length = 2
 }
 
-data "aws_ami" "this" {
+data "aws_ami" "ubuntu" {
   most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
+
   filter {
     name   = "name"
-    values = ["al2023-ami-2023*"]
+    values = ["ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
 }
 
-resource "aws_instance" "this" {
-  ami = data.aws_ami.this.id
-  instance_market_options {
-    spot_options {
-      max_price = 0.0031
-    }
-  }
-  instance_type = "t4g.nano"
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
   tags = {
-    Name = "test-spot"
+    Name = "HelloWorld"
   }
-}
